d8432bb0e18ffb13fc7267f06950a6ed3e807c2b
suspect <mail@elias-blu.me>
2022-08-06 02:35:42 +0200

    remove unused export

--- libs/mailjet/src/mailjet.module-definitons.ts
+++ libs/mailjet/src/mailjet.module-definitons.ts
@@ -1,5 +1,5 @@
 import { ConfigurableModuleBuilder } from '@nestjs/common'
 import { MailjetModuleOptions } from './interfaces/mailjet-module-options.interface'
 
-export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN, OPTIONS_TYPE, ASYNC_OPTIONS_TYPE } =
+export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } =
     new ConfigurableModuleBuilder<MailjetModuleOptions>().build()
